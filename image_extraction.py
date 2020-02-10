ef41b5774d7ef8e0476874a7a42294ca728971dc
evansavage <esav678@gmail.com>
2020-02-10 13:11:36 -0500

    Added page_maker, mapping files, edited image_extraction

--- image_extraction.py
+++ image_extraction.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import pandas as pd
 import cv2
 from PIL import Image
 import xml.etree.ElementTree as ET
@@ -13,90 +14,126 @@ import re
 image_path = 'PNG_compressed'
 mei_path_split = '180829_split/mei'
 mei_path_unsplit = '180831_unsplit/mei'
+salz_path = 'salz'
+mapping_file = 'img_name_to_cantus_mapping.csv'
 output_path = 'output_img'
 output_txt = 'output_xml'
+# os.system(f'rm -rf { output_path }')
+# os.system(f'rm -rf { output_txt }')
+
+mapping = pd.read_csv(mapping_file)
+entries = mapping.shape[0]
+
 os.system(f'rm -rf { output_path }')
-os.system(f'rm -rf { output_txt }')
 
 if not os.path.isdir(output_path):
     os.mkdir(output_path)
+
 if not os.path.isdir(output_txt):
     os.mkdir(output_txt)
 
-for mei_file in sorted(os.listdir(mei_path_unsplit)):
-    if mei_file.endswith('.DS_Store'):
-        continue
-    print(image_path + '/' + mei_file[:-4] + '.png')
-    img = Image.open(image_path + '/' + mei_file[:-4] + '.png')
-    print(mei_path_unsplit + '/' + mei_file)
-    contents = open(mei_path_unsplit + '/' + mei_file, 'r', encoding='utf8')
-    soup = BeautifulSoup(contents, 'xml')
-    staffs = soup.find_all('staff')
-    clefs = soup.find_all('clef')
-    custos = soup.find_all('custos')
-    comments = list(soup.find_all(string=lambda text: isinstance(text, Comment)))
-
-    staff_facs = []
-    clef_facs = []
-    clef_shapes = []
-    custos_facs = []
-    clean_comments = []
-
-    for staff in staffs:
-        staff_facs.append(staff['facs'])
-    for clef in clefs:
-        clef_facs.append(clef['facs'])
-        clef_shapes.append(clef['shape'])
-    for c in custos:
-        custos_facs.append(c['facs'])
-    for comment in comments:
-        comment = re.sub(r'\.[a-zA-Z][0-9]\.', ', ', comment)
-        for neume in comment.split(', '):
-            clean_comments.append(neume)
-    print('Neumes:', len(clean_comments) + len(clefs) + len(custos))
-    # oblique repeat removal
-
-    temp_zone = BeautifulSoup('<zone lrx="-1" lry="-1" ulx="-1" uly="-1" xml:id="q-5d0f64d9-f3ba-4ddd-bc4c-9192cdbe0d1f"/>').zone
-
-    for zone in soup.find_all('zone'):
-        if (temp_zone['ulx'] == zone['ulx'] and temp_zone['lry'] == zone['lry']) or zone['xml:id'] in staff_facs:
-            zone['delete'] = 1
-        temp_zone = zone
-
-    for zone in soup.find_all('zone'):
-        if zone.has_attr('delete'):
-            zone.decompose()
-
-    zones = soup.find_all('zone')
-    print('Zones:', len(zones))
-
-
-
-    # print(type(comments))
-    # REGEX COMMENT CLEANUP: \.[a-zA-Z][0-9]\. (might not need)
-
-
-
-    if not os.path.isdir(output_path + '/' + mei_file[:-4]):
-        os.mkdir(output_path + '/' + mei_file[:-4])
-    zone_count = 0
-    nc_inc = 0
-    with open(f'{ output_txt }/{ mei_file[:-4] }.txt', 'w') as f:
-        f.write('file_name,type\n')
+with open(f'output.txt', 'w') as f:
+    f.write('file_name,type\n')
+    for i, mei_file in enumerate(sorted(os.listdir(salz_path))):
+        if mei_file.endswith('.DS_Store'):
+            continue
+        elif i > entries - 1:
+            break
+        mapped_file = mapping.iloc[i]['filename']
+        # print(image_path + '/' + mei_file[:-4] + '.png')
+        img = Image.open(image_path + '/' + mapped_file + '.png')
+        # print(mei_path_unsplit + '/' + mei_file)
+        contents = open(salz_path + '/' + mei_file, 'r', encoding='utf8').read()
+        soup = BeautifulSoup(contents, 'xml')
+        # staffs = soup.find_all('staff')
+        neumes = soup.find_all(['neume', 'clef', 'custos'])
+        zones = soup.find_all('zone')
+        i = 0
         for zone in zones:
             fac = zone['xml:id']
-            if fac in clef_facs:
-                ind = clef_facs.index(fac)
-                if clef_shapes[ind] == 'C':
-                    f.write(f'{ mei_file[:-4] }_{ zone_count }.png,clef.c\n')
-                else:
-                    f.write(f'{ mei_file[:-4] }_{ zone_count }.png,clef.f\n')
-            elif fac in custos_facs:
-                f.write(f'{ mei_file[:-4] }_{ zone_count }.png,custos\n')
-            else:
-                f.write(f'{ mei_file[:-4] }_{ zone_count }.png,{ clean_comments[nc_inc] }\n')
-                nc_inc += 1
-            zone_img = img.crop((int(zone['ulx']),int(zone['uly'])-15,int(zone['lrx']),int(zone['lry'])+15))
-            zone_img.save(f'{ output_path }/{ mei_file[:-4] }/{ mei_file[:-4] }_{ zone_count }.png')
-            zone_count += 1
-    # tree = ET.parse(mei_path + '/' + mei_file)
+            if int(zone['lrx']) - int(zone['ulx']) > 10:
+                for neume in neumes:
+                    neume_type = ''
+                    if neume['facs'] == fac:
+                        zone_img = img.crop((int(zone['ulx']),int(zone['uly'])-15,int(zone['lrx']),int(zone['lry'])+15))
+                        zone_img.save(f'{ output_path }/{ mei_file[:-4] }_{ i }.png')
+                        neume_type = 'hold'
+                        if neume.has_attr('name'):
+                            if neume.findChild().has_attr('inclinatum'):
+                                neume_type = 'inclinatum'
+                            else:
+                                neume_type = neume['name']
+                        elif neume.has_attr('shape'):
+                            neume_type = 'clef.' + neume['shape'].lower()
+                        elif neume.has_attr('pname'):
+                            neume_type = 'custos'
+
+                        f.write(f'{ mei_file[:-4] }_{ i },{ neume_type }\n')
+                        i += 1
+
+        # comments = list(soup.find_all(string=lambda text: isinstance(text, Comment)))
+        #
+        # staff_facs = []
+        neumes_facs = []
+        clef_facs = []
+        # clef_shapes = []
+        custos_facs = []
+        # clean_comments = []
+        #
+        # for staff in staffs:
+        #     staff_facs.append(staff['facs'])
+        # for clef in clefs:
+        #     clef_facs.append(clef['facs'])
+        #     clef_shapes.append(clef['shape'])
+        # for c in custos:
+        #     custos_facs.append(c['facs'])
+        # for comment in comments:
+        #     comment = re.sub(r'\.[a-zA-Z][0-9]\.', ', ', comment)
+        #     for neume in comment.split(', '):
+        #         clean_comments.append(neume)
+        # print('Neumes:', len(clean_comments) + len(clefs) + len(custos))
+        # # oblique repeat removal
+        #
+        # temp_zone = BeautifulSoup('<zone lrx="-1" lry="-1" ulx="-1" uly="-1" xml:id="q-5d0f64d9-f3ba-4ddd-bc4c-9192cdbe0d1f"/>').zone
+        #
+        # for zone in soup.find_all('zone'):
+        #     if (temp_zone['ulx'] == zone['ulx'] and temp_zone['lry'] == zone['lry']) or zone['xml:id'] in staff_facs:
+        #         zone['delete'] = 1
+        #     temp_zone = zone
+        #
+        # for zone in soup.find_all('zone'):
+        #     if zone.has_attr('delete'):
+        #         zone.decompose()
+        #
+        # zones = soup.find_all('zone')
+        # print('Zones:', len(zones))
+        #
+        #
+        #
+        # # print(type(comments))
+        # # REGEX COMMENT CLEANUP: \.[a-zA-Z][0-9]\. (might not need)
+        #
+        #
+        #
+        # # if not os.path.isdir(output_path + '/' + mei_file[:-4]):
+        # #     os.mkdir(output_path + '/' + mei_file[:-4])
+        # zone_count = 0
+        # nc_inc = 0
+        #
+        # for zone in zones:
+        #     fac = zone['xml:id']
+        #     if fac in clef_facs:
+        #         ind = clef_facs.index(fac)
+        #         if clef_shapes[ind] == 'C':
+        #             f.write(f'{ mei_file[:-4] }_{ zone_count }.png,clef.c\n')
+        #         else:
+        #             f.write(f'{ mei_file[:-4] }_{ zone_count }.png,clef.f\n')
+        #     elif fac in custos_facs:
+        #         f.write(f'{ mei_file[:-4] }_{ zone_count }.png,custos\n')
+        #     else:
+        #         f.write(f'{ mei_file[:-4] }_{ zone_count }.png,{ clean_comments[nc_inc] }\n')
+        #         nc_inc += 1
+        #     zone_img = img.crop((int(zone['ulx']),int(zone['uly'])-15,int(zone['lrx']),int(zone['lry'])+15))
+        #     zone_img.save(f'{ output_path }/{ mei_file[:-4] }_{ zone_count }.png')
+        #     zone_count += 1
+        # tree = ET.parse(mei_path + '/' + mei_file)
---
+++ page_maker.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import numpy as np
+import cv2
+import pandas as pd
+from PIL import Image
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+from bs4 import BeautifulSoup
+from bs4 import Comment
+import re
+import random
+
+print('How many staffs do you want on a page?')
+num_staffs = int(input())
+
+page_width = 2000
+page_height = 3000
+top_offset = 200
+bottom_offset = 500
+margin_left = 200
+margin_right = 200
+staff_spacing = 100
+staff_image = Image.open('fake_staff_lines.png', 'r')
+
+neume_path = 'output_img/'
+neume_info = pd.read_csv('output.txt')
+neume_info = neume_info.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# print(neume_info)
+
+
+staff_width_orig, staff_height_orig = staff_image.size
+
+# df.loc[df['column_name'].isin(some_values)]
+
+neumes = neume_info.loc[~neume_info['type'].isin(['clef.c', 'clef.f', 'custos'])]
+clefs = neume_info.loc[neume_info['type'].isin(['clef.c', 'clef.f'])]
+custos_arr = neume_info.loc[neume_info['type'] == 'custos']
+print(custos_arr)
+staff_scale = 100 / staff_height_orig
+# staff_image.thumbnail((staff_width_orig*staff_scale, staff_height_orig*staff_scale), Image.ANTIALIAS)
+blank_image = Image.new('RGBA', (page_width, page_height), (232, 228, 220, 255))
+staff_coords = []
+for i in range(num_staffs):
+    if top_offset + int(0.33 * i * staff_width_orig*staff_scale) > page_height - bottom_offset:
+        break
+    neume_name = random.choice(neume_info['file_name'])
+    neume_image = Image.open(neume_path + neume_name + '.png', 'r')
+    neume_image = neume_image.crop((0,15, neume_image.size[0], neume_image.size[1] - 15))
+
+    staff_scale_height = staff_scale * random.uniform(0.93, 1.07)
+    staff_scale_width = random.uniform(0.97, 1.03)
+    staff_place = staff_image.resize((int(staff_scale_width * (page_width - margin_left - margin_right)), int(staff_height_orig*staff_scale_height)), Image.ANTIALIAS)
+    w, h = staff_place.size
+    pos_offset = int(h / 6)
+    margin_left_offset = random.randint(-10,10)
+    blank_image.paste(staff_place, (margin_left + margin_left_offset , top_offset + int(0.33 * i * staff_width_orig*staff_scale)), mask=staff_place)
+    num_neumes = random.randint(15, 30)
+    for j in range(num_neumes):
+        if j == 0: # 
+            choice = clefs.sample(n=1)
+            choice_file, choice_type = choice.iloc[0]
+            n_place = random.choice([0, 2, 4])
+            scale = 2/3 + 0.05
+        elif j == num_neumes - 1:
+            choice = custos_arr.sample(n=1)
+            choice_file, choice_type = choice.iloc[0]
+            n_place = random.randint(0,8)
+            scale = 1/3
+        else:
+            choice = neumes.sample(n=1)
+            choice_file, choice_type = choice.iloc[0]
+            if choice_type == 'oblique2':
+                n_place = random.randint(0,7)
+                scale = 1/2
+            elif choice_type == 'oblique3':
+                n_place = random.randint(0,6)
+                scale = 2/3
+            elif choice_type == 'oblique4':
+                n_place = random.randint(0,5)
+                scale = 5/6
+            else:
+                n_place = random.randint(0,8)
+                scale = 1/3
+        n_image = Image.open(neume_path + choice_file  + '.png')
+        n_image = n_image.crop((0,15, n_image.size[0], n_image.size[1] - 15))
+        n_ratio = n_image.size[0] / n_image.size[1]
+        n_image = n_image.resize((int(scale*h*n_ratio), int(scale*h)), Image.ANTIALIAS)
+        blank_image.paste(n_image, (margin_left + margin_left_offset + int(j * w / num_neumes), top_offset + int(0.33 * i * staff_width_orig*staff_scale) + int(-2*pos_offset + n_place*pos_offset) + random.randint(-5,5)))
+
+    # print(w, h, pos_offset)
+    # blank_image.paste(neume_image, (random.randint(0,page_width), random.randint(0, page_height)))
+
+
+blank_image.show()
+cv2.waitKey()
